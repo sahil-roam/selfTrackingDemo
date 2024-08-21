7bc0cec2601bd13f636db64f844215aaab762d6a
sahil-roam <97589523+sahil-roam@users.noreply.github.com>
2024-08-21 13:07:23 +0530

    listener

--- index.js
+++ index.js
@@ -6,4 +6,12 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+const startListener = () => {
+    Roam.startListener('location', locations => {
+        console.log(JSON.stringify(locations))
+    })
+}
+
 AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerHeadlessTask('RoamHeadlessService', startListener())
+
