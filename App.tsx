469adfe1f6134ab0f0abcdb229442f64926b5cd3
sahil-roam <97589523+sahil-roam@users.noreply.github.com>
2024-08-21 12:55:32 +0530

    self tracking init

--- App.tsx
+++ App.tsx
@@ -15,6 +15,8 @@ import {
   Text,
   useColorScheme,
   View,
+  TouchableHighlight,
+  Platform
 } from 'react-native';
 
 import {
@@ -24,36 +26,9 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import Roam from 'roam-reactnative';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -62,57 +37,99 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const requestPermission = () => {
+    Roam.requestLocationPermission()
+  }
+
+  const requestBackgroundLocationPermission = () => {
+    Roam.requestBackgroundLocationPermission()
+  }
+
+  const startTracking = () => {
+
+    if(Platform.OS === 'android'){
+      Roam.startTracking(Roam.TrackingMode.ACTIVE)
+      //Roam.startTracking(Roam.TrackingMode.BALANCED)
+      //Roam.startTracking(Roam.TrackingMode.PASSIVE)
+      //Roam.startSelfTrackingDistanceInterval(5, 20, 50)
+      //Roam.startSelfTrackingTimeInterval(5, 50)
+    } else {
+      Roam.startTracking(Roam.TrackingMode.ACTIVE)
+      //Roam.startTracking(Roam.TrackingMode.BALANCED)
+      //Roam.startTracking(Roam.TrackingMode.PASSIVE)
+      // Roam.startTrackingCustom(
+      //   true,
+      //   false,
+      //   Roam.ActivityType.FITNESS,
+      //   Roam.DesiredAccuracyIOS.BEST,
+      //   true,
+      //   5,
+      //   50,
+      //   5
+      //   )
+    }
+
+
+  }
+
+
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+      <ScrollView>
+      <TouchableHighlight 
+      style={styles.button1}
+      onPress={() => {requestPermission()}}
+      >
+        <Text>Location Permission</Text>
+      </TouchableHighlight>
+
+      {
+        Platform.OS === 'android'
+        ?
+        <TouchableHighlight 
+      style={styles.button1}
+      onPress={() => {requestBackgroundLocationPermission()}}
+      >
+        <Text>Background Location Permission</Text>
+      </TouchableHighlight>
+      : <View></View>
+      }
+
+      
+      <TouchableHighlight 
+      style={styles.button1}
+      onPress={() => {startTracking()}}
+      >
+        <Text>Start Tracking</Text>
+      </TouchableHighlight>
+
+      <TouchableHighlight 
+      style={styles.button1}
+      onPress={() => {Roam.stopTracking()}}
+      >
+        <Text>Stop Tracking</Text>
+      </TouchableHighlight>
+
+     
       </ScrollView>
+      
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   sectionContainer: {
-    marginTop: 32,
+    marginTop: 20,
     paddingHorizontal: 24,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  button1: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginBottom: 20,
+    color: 'blue',
+    alignSelf: 'center'
+  }
+  
 });
 
 export default App;
